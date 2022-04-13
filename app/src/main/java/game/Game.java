cca964f67b1394cdb800271dcbf59431b6bf2cc6
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-04-13 14:21:26 +0100

    random word and remaining attempts methods completed

--- app/src/main/java/game/Game.java
+++ app/src/main/java/game/Game.java
@@ -1,11 +1,21 @@
 package game;
 
+import java.util.Random;
+
 public class Game {
 
   String word;
+  int attemptsRemaining = 10;
 
-  Game(String inputWord) {
-    word = inputWord;
+  public static final String[] DICTIONARY= {
+    "MAKERS",
+    "CANDIES",
+    "DEVELOPER",
+    "LONDON",
+  };
+
+  Game() {
+    word = getRandomWordFromDictionary();
   }
 
   public String getWordToGuess() {
@@ -21,8 +31,19 @@ public class Game {
     String result = sb.toString();
     return result;
   }
+
+  public int getRemainingAttempts() {
+    return attemptsRemaining;
+  }
+
+  public static String getRandomWordFromDictionary() {
+    Random rand = new Random();
+    Integer randomWord = rand.nextInt(DICTIONARY.length);
+    return randomWord.toString();
+  }
+
   public static void main(String[] args) {
-    Game game = new Game("MAKERS");
+    Game game = new Game();
     String guess = game.getWordToGuess();
     System.out.println(guess);
   }
--- app/src/test/java/game/GameTest.java
+++ app/src/test/java/game/GameTest.java
@@ -1,12 +1,31 @@
 package game;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class GameTest {
-  @Test public void testGetsWordToGuess() {
-    Game game = new Game("MAKERS");
-    assertEquals(game.getWordToGuess(), "M_____");
+  // @Test public void testGetsWordToGuess() {
+  //   Game game = new Game();
+  //   assertEquals(game.getWordToGuess(), "M_____");
+  // }
+
+  @Test public void testGetsInitialRemainingAttempts() {
+    Game game = new Game();
+    assertEquals(game, Integer.valueOf(10));
+  }
+
+  @Test public void testGetRandomWordFromDictionary() {
+    String word = Game.getRandomWordFromDictionary();
+    boolean foundIt = false;
+    for (String dictWord : Game.DICTIONARY)
+    {
+      if (word.equals(dictWord))
+    {
+      foundIt = true;
+    }
+    }
+    assertTrue(foundIt);
   }
 }
