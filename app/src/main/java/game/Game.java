ac52695fdc8d9c1d86045a5f25d3a5c7d9deea0c
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-04-13 12:01:37 +0100

    created for loop in getWordToGuess method

--- app/src/main/java/game/Game.java
+++ app/src/main/java/game/Game.java
@@ -2,10 +2,28 @@ package game;
 
 public class Game {
 
+  String word;
+
+  Game(String inputWord) {
+    word = inputWord;
+  }
+
   public String getWordToGuess() {
-    return "B_____";
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < word.length(); i++){
+      char c = word.charAt(i);
+      if (i != 0) {
+        sb.append("_");
+      } else {
+        sb.append(c);
+      }         
+    }
+    String result = sb.toString();
+    return result;
   }
   public static void main(String[] args) {
-
+    Game game = new Game("MAKERS");
+    String guess = game.getWordToGuess();
+    System.out.println(guess);
   }
 }
